adddbdd7d6ef7cde87179bce3cdf24a61358b886
YONG JUN CHOI <67721584+codeCYJ@users.noreply.github.com>
2023-06-27 14:52:19 +0900

    Update Router.js

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,17 +1,17 @@
 
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import ReactionRate from "./pages/ReactionRate";
-import DinosaurGame from "./pages/DinosaurGame";
-import Tetris from "./pages/Tetris";
+import ReactionRate from "components/pages/ReactionRate";
+import DinosaurGame from "components/pages/DinosaurGame";
+import Tetris from "components/pages/Tetris";
 
 const Routers = () => {
 
   return (
     <Router>
       <Routes>
-            <Route path="/reaction_rate" element={<ReactionRate/>}/>
-            <Route path="/dinosaur_game" element={<DinosaurGame/>}/>
-            <Route path="/tetris" element={<Tetris/>}/>
+        <Route path="/reaction_rate" element={<ReactionRate/>}/>
+        <Route path="/dinosaur_game" element={<DinosaurGame/>}/>
+          <Route path="/tetris" element={<Tetris/>}/>
       </Routes>
     </Router>
   );
